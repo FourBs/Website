cf502799460e1bc9582913dfef767a9afdda86fc
Jaakko Jokinen <jaakko.jokinen@outlook.com>
2018-05-14 17:42:40 +0300

    Remove gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,5 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
-  pathPrefix: '/website'
+  plugins: ['gatsby-plugin-react-helmet']
 }
