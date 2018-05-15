79e10cf68f7618b3f70cf5bd258b8463dd48ecaa
Jaakko Jokinen <jaakko.jokinen@outlook.com>
2018-05-15 11:30:11 +0300

    New homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,25 @@ import React from 'react'
 import styled, { keyframes } from 'styled-components'
 import Link from 'gatsby-link'
 
+import {
+  BigTitle,
+} from '../components'
 import treeIllustration from '../svg/tree-illustration.svg'
 
 const Section = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: stretch;
   width: 100%;
 `
 const TreeSection = Section.extend`
+  flex-grow: 1;
   justify-content: center;
   position: relative;
-  height: 80vh;
+  margin-top: -3rem;
+  height: calc(100% + 3rem);
+
+  background: #020202;
 `
 const TreeImage = styled.img`
   position: absolute;
@@ -21,17 +28,17 @@ const TreeImage = styled.img`
   top: -10vh;
   margin: 0 auto;
 
-  opacity: .9;
+  opacity: .0;
 `
 const PulseAnimation = keyframes`
   0% {
-    box-shadow: 0 0 0 8vh rgba(116, 194, 116, 0.4);
+    box-shadow: 0 0 0 2vh rgba(247, 241, 86, .84);
   }
   50% {
-    box-shadow: 0 0 0 9vh rgba(116, 194, 116, 0.4);
+    box-shadow: 0 0 0 2.2vh rgba(247, 241, 86, .84);
   }
   100% {
-    box-shadow: 0 0 0 8vh rgba(116, 194, 116, 0.4);
+    box-shadow: 0 0 0 2vh rgba(247, 241, 86, .84);
   }
 `
 const ContentContainer = styled.div`
@@ -47,11 +54,11 @@ const ContentContainer = styled.div`
     content: ' ';
     position: absolute;
     display: block;
-    height: 65vh;
-    width: 65vh;
+    height: 72vh;
+    width: 72vh;
 
-    background: rgba(86, 152, 84, 0.98);
-    box-shadow: 0 0 0 6vh rgba(116, 194, 116, 0.4);
+    background: rgba(10, 10, 10, .9);
+    box-shadow: 0 0 0 2vh rgba(247, 241, 86, .84);
     border-radius: 100%;
 
     animation: ${ PulseAnimation } 4s ease-in infinite;
@@ -62,12 +69,24 @@ const Tagline = styled.h2`
   flex-grow: 0;
   flex-shrink: 1;
   display: inline;
-  margin-bottom: 3rem;
+  margin-bottom: 1rem;
 
   font-size: 2.4rem;
   font-weight: 900;
   text-align: center;
   color: #fff;
+`
+const TagCompanion = styled.p`
+  position: relative;
+  max-width: 50vh;
+  margin-top: 0;
+
+  text-align: center;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+  Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  color: #fafafa;
+  font-size: 1.2rem;
+  font-weight: 300;
 `
 const ButtonContainer = styled.div`
   display: flex;
@@ -77,15 +96,15 @@ const BigLink = styled(Link)`
   padding: 1.6rem 3rem;
   margin-right: 3rem;
 
-  color: #578a3d;
+  color: #020202;
   text-decoration: none;
   font-family: sans-serif;
   font-weight: 600;
   font-size: 1.1rem;
 
-  background: #fff;
+  background: rgb(246, 241, 87);
   border-radius: 6px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, .14);
+  box-shadow: 0 4px 8px rgba(0, 0, 0, .2);
   transition: all .14s ease-out;
 
   &:last-child {
@@ -94,8 +113,17 @@ const BigLink = styled(Link)`
 
   &:hover {
     transform: translateY(-1px);
-    box-shadow: 0 8px 14px rgba(0, 0, 0, .16);
+    box-shadow: 0 8px 14px rgba(0, 0, 0, .26);
   }
+`
+const BigTitleM = BigTitle.extend`
+  position: relative;
+  max-width: 50vh;
+  margin-top: 0;
+
+  text-align: center;
+  color: #bbb;
+  line-height: 1.3;
 `
 
 const IndexPage = () => (
@@ -104,6 +132,7 @@ const IndexPage = () => (
       <TreeImage src={ treeIllustration } />
       <ContentContainer>
         <Tagline>Let's grow together</Tagline>
+        <BigTitleM>Our platform connects learners with industry and education.</BigTitleM>
         <ButtonContainer>
           <BigLink to={ '/join-us' }>Learners →</BigLink>
           <BigLink to={ '/partners' }>Educators →</BigLink>
