918275af02e6c12dcedbc9ecbf3566313a922e8b
Jaakko Jokinen <jaakko.jokinen@outlook.com>
2018-05-21 13:29:15 +0300

    Add partners in partners view

--- src/components/layout/MaxWidth.js
+++ src/components/layout/MaxWidth.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const MaxWidth = styled.div`
   display: flex;
+  flex-direction: ${ props => props.flexDirection ? props.flexDirection : 'inherit' };
   align-items: center;
   width: 100%;
   max-width: 1100px;
--- src/pages/partners.js
+++ src/pages/partners.js
@@ -11,11 +11,54 @@ import {
   BigPara,
 } from '../components'
 
+import UNICEFLogo from '../svg/UNICEF_Logo.png'
+import OECDLogo from '../svg/OECD_logo_new.svg'
+
+const Partners = styled.div`
+  padding: 2rem 0 3rem;
+  margin-bottom: 2rem;
+
+  //background: #f4f4f4;
+  border-top: 1px solid #e3e3e3;
+  border-bottom: 1px solid #e3e3e3;
+`
+const H3 = styled.h3`
+  width: 100%;
+  margin-bottom: 2rem;
+
+  font-size: 1.3rem;
+  font-weight: 300; 
+  text-align: center;
+  color: #757570;
+`
+const LogoRow = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const Logo = styled.div`
+  width: 200px;
+  margin-right: 2rem;
+
+  font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen, Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
+  font-weight: 700;
+  text-align: center;
+  color: #52534e;
+
+  &:last-child {
+    margin-right: 0;
+  }
+
+  & img {
+    margin-bottom: 0;
+  }
+`
+
 const PartnersPage = () => (
   <div style={{ width: '100%' }}>
     <MaxWidth>
       <TitleContainer>
-        <BigTitle><Strong>Together we will grow</Strong></BigTitle>
+        <BigTitle><Strong>Together</Strong> we will grow</BigTitle>
       </TitleContainer>
     </MaxWidth>
     <MaxWidth>
@@ -24,6 +67,17 @@ const PartnersPage = () => (
         <BigPara alignSelf='flex-end'>Through our platform you can reach new customers and partners in a given location. By boosting the local economy you profit and increase your influence.</BigPara>
       </ContentContainer>
     </MaxWidth>
+    <Partners>
+      <MaxWidth flexDirection={ 'column' }>
+        <H3>Partners</H3>
+        <LogoRow>
+          <Logo><img src={ UNICEFLogo } /></Logo>
+          <Logo><img src={ OECDLogo } /></Logo>
+          <Logo>Agency for Industry and Commerce</Logo>
+          <Logo>Ministry of Education</Logo>
+        </LogoRow>
+      </MaxWidth>
+    </Partners>
   </div>
 )
 
